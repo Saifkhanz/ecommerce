0c15de5a7f94d9c09f690126566aaf25a478710e
SaifKhanz <saif137.khan@gmail.com>
2022-09-08 20:23:20 +0530

    some changes

--- src/components/cartPage.vue
+++ src/components/cartPage.vue
@@ -123,6 +123,15 @@
               </div> 
                
              <div class="flex flex-row justify-between">
+                
+            <button v-on:click="remove(data)"  class="bg-gradient-to-r from bg-red-600 to bg-pink-700 rounded-full py-2 px-4 text-gray-50 flex flex-row text-sm
+            hover:from-pink-600 hover:to-pink-600 ">
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1 ">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+              Remove Item 
+            </button>
+
                  <button v-on:click="item_fun(data.id)" class="bg-blue-600 rounded-full py-2 px-4 text-gray-50 flex flex-row 
                 hover:bg-purple-700 text-sm">
                   Place order
@@ -150,15 +159,23 @@ name:"cartPage",
   data()
   {
     return{
-       temp :""
+       temp :"",
+       len:0
     }
   },
   created(){
       
-  
       this.temp = localStorage.getItem("products_data");
              this.temp = JSON.parse(this.temp);
     
+  },
+
+  updated(itemData)
+  {
+    var productArray = JSON.parse(localStorage.getItem("products_data"));
+        productArray.splice(productArray.findIndex(a => a.id === itemData.id) , 1);
+        console.warn(productArray);
+        localStorage.setItem("products_data",JSON.stringify(productArray));
   },
  methods:{
     logOut() {
@@ -166,12 +183,55 @@ name:"cartPage",
       localStorage.clear();
       this.$router.push({ name: "Login" });
     },
+       remove(itemData){
+       
+        alert(itemData.id)
+       
+        var productArray = JSON.parse(localStorage.getItem("products_data"));
+        productArray.splice(productArray.findIndex(a => a.id === itemData.id) , 1);
+        console.warn(productArray);
+        localStorage.setItem("products_data",JSON.stringify(productArray));
+
+        //  this.len=this.temp.length
+        //  alert(this.len);
+          // for(let i=0;i<this.len;i++)
+         //  {
+
+
+    /*function and productArrayay filter method 
+
+
+          y=this.temp.filter(itemRemove);
+        function itemRemove(itemData,i=-1)
+          {
+            i++;   
+            alert(i)
+         alert(this.temp[1].id)
+         console.warn(this.temp[1])
+         return itemData.id!==this.temp[1].id
+          }
+
+
+         */
+
+
+      //   y=this.temp.filter((itemData,i=0)=>{
+      //    alert(i)
+      //    alert(this.temp[i].id)
+      //    return itemData.id!==this.temp[i].id;
+      //     console.warn(this.temp[i])
+      //   i++;
+      // })
+  
+           //}
+      
+       },
     item_fun(rId) {
-            let temp, x;
-            temp = localStorage.getItem("products_details");
-            temp = JSON.parse(temp);
-            x = temp[rId - 1].title;
-            console.warn(temp[rId - 1].title);
+            let tempo, x;
+            tempo = localStorage.getItem("products_details");
+            tempo = JSON.parse(tempo);
+            x = tempo[rId - 1].title;
+            console.warn(tempo[rId - 1].title);
             if(confirm("Are you confirm want to placed this order",x))
            {
              alert("your Order is Successfully placed");
